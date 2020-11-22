fe513e06439b2d070b33ff0a864607016273bfc5
satishtrip <satish.tripathi1297@gmail.com>
2020-11-23 02:27:59 +0530

    url and page sources

--- automate.py
+++ automate.py
@@ -4,5 +4,7 @@ from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome(executable_path="C:\Program Files\Driver\chromedriver")
 driver.get("https://www.youtube.com/")
 print(driver.title)
+print(driver.current_url)
+print(driver.page_source)
 driver.implicitly_wait(1000)
 driver.close()
