7959010cc651a5ad227e7efd0f0654d92718e445
satishtrip <satish.tripathi1297@gmail.com>
2020-11-24 04:12:56 +0530

    url and page sources

--- automate.py
+++ automate.py
@@ -1,10 +1,30 @@
 from selenium import webdriver
+import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+import re
+import json
+
 
 from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome(executable_path="C:\Program Files\Driver\chromedriver")
-driver.get("https://www.youtube.com/")
-print(driver.title)
-print(driver.current_url)
-print(driver.page_source)
-driver.implicitly_wait(1000)
-driver.close()
+driver.get("https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin")
+user_elem = driver.find_element_by_name("session_key")
+print(user_elem.is_displayed())
+print(user_elem.is_enabled())
+
+pwd_elem = driver.find_element_by_name("session_password")
+print(pwd_elem.is_displayed())
+print(pwd_elem.is_enabled())
+
+user_elem.send_keys("session_key")
+
+pwd_elem.send_keys("session_password")
+
+
+
+pwd_elem.submit()
