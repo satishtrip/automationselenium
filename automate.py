37659e20ea48ad6a1569b78f5099d1c6cd9d70de
satishtrip <satish.tripathi1297@gmail.com>
2020-11-25 03:46:58 +0530

    Login linkedln and click on jobs

--- automate.py
+++ automate.py
@@ -13,6 +13,8 @@ import json
 from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome(executable_path="C:\Program Files\Driver\chromedriver")
 driver.get("https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin")
+
+driver.maximize_window()
 user_elem = driver.find_element_by_name("session_key")
 print(user_elem.is_displayed())
 print(user_elem.is_enabled())
@@ -21,10 +23,17 @@ pwd_elem = driver.find_element_by_name("session_password")
 print(pwd_elem.is_displayed())
 print(pwd_elem.is_enabled())
 
-user_elem.send_keys("session_key")
+user_elem.send_keys("") #usn
 
-pwd_elem.send_keys("session_password")
+pwd_elem.send_keys("")  # pwd
+
+
+
 
 
 
 pwd_elem.submit()
+
+driver.find_element_by_id("ember26").click()   # clicks on jobs icon
+
+driver.find_element(By.ID,"jobs-search-box-location-id-ember991").send_keys("Bangalore")
